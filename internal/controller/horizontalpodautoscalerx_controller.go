6e28f11cee0093a282c70d27564c8af3e2f1882b
Adam P. Regasz-Rethy <adam.regaszrethy@shopify.com>
2025-04-11 22:59:44 -0400

    Get the associated hpa

--- internal/controller/horizontalpodautoscalerx_controller.go
+++ internal/controller/horizontalpodautoscalerx_controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"context"
 
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -15,6 +16,7 @@ import (
 
 const (
 	ControllerName = "horizontalpodautoscalerx"
+	requeueAfter   = 30
 )
 
 // HorizontalPodAutoscalerXReconciler reconciles a HorizontalPodAutoscalerX object
@@ -36,9 +38,27 @@ type HorizontalPodAutoscalerXReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.18.2/pkg/reconcile
 // This Reconcile method uses the ObjectReconciler interface from https://github.com/kubernetes-sigs/controller-runtime/pull/2592.
 func (r *HorizontalPodAutoscalerXReconciler) Reconcile(ctx context.Context, hpax *autoscalingxv1.HorizontalPodAutoscalerX) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	if !hpax.DeletionTimestamp.IsZero() {
+		// The object is being deleted, don't do anything.
+		return ctrl.Result{}, nil
+	}
+
+	_, err := r.getHPA(ctx, hpax)
+	if err != nil {
+		log.Error(err, "getting associated HPA")
+		return ctrl.Result{
+			RequeueAfter: requeueAfter,
+		}, nil
+	}
+
+	defer func() {
+		err := r.Status().Update(ctx, hpax)
+		if err != nil {
+			log.Error(err, "updating status")
+		}
+	}()
 
 	return ctrl.Result{}, nil
 }
@@ -50,4 +70,13 @@ func (r *HorizontalPodAutoscalerXReconciler) SetupWithManager(mgr ctrl.Manager) 
 		Named(ControllerName).
 		WithEventFilter(predicate.Or(predicate.GenerationChangedPredicate{}, predicate.AnnotationChangedPredicate{})).
 		Complete(reconcile.AsReconciler(mgr.GetClient(), r))
+}
+
+func (r *HorizontalPodAutoscalerXReconciler) getHPA(ctx context.Context, hpax *autoscalingxv1.HorizontalPodAutoscalerX) (*autoscalingv1.HorizontalPodAutoscaler, error) {
+	hpa := &autoscalingv1.HorizontalPodAutoscaler{}
+	err := r.Get(ctx, client.ObjectKey{Name: hpax.Spec.HPATargetName, Namespace: hpax.Namespace}, hpa)
+	if err != nil {
+		return nil, err
+	}
+	return hpa, nil
 }
